f99bf5c5a8106a6889a5199c51ca1ccdb0832821
yoshl <108870244+Llo0zy@users.noreply.github.com>
2023-08-11 21:36:02 +0200

    Create volume_down.sh

---
+++ volume_down.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_volume=$(amixer -D pulse sget Master | grep -oE '[0-9]+%' | sed -n 1p | tr -d '%')
+step=5
+
+new_volume=$((current_volume - step))
+if [ "$new_volume" -lt 0 ]; then
+    new_volume=0
+fi
+
+amixer -D pulse sset Master "${new_volume}%" &>/dev/null
+notify-send -i audio-volume-low -h int:value:${new_volume} -t 1000 "Volumen: ${new_volume}%"
